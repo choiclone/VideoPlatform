abe0a3bab117aae9e0d3ba1d11043f10e6f927c7
choiclone <s207040@gmail.com>
2022-08-31 14:35:32 +0900

    수정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  distDir: 'build',
+  // distDir: 'build',
   env: {
     BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },

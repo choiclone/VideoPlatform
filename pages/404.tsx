bf84cdc21dd2e179d50aa111a0a65795865dd741
choiclone <s207040@gmail.com>
2022-09-01 10:14:37 +0900

    404 페이지 추가

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error404 = () => {
+    return (
+        <div className='flex flex-col items-center justify-center w-full h-full'>
+            <h1 className='font-bold'>404 - Not Found Page</h1>
+        </div>
+    )
+}
+
+export default Error404
